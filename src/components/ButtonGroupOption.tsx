fbbe9bb75235c0a26889f5b80493c88929a06aa5
lucastllima <lucasteixeira-lima@hotmail.com>
2023-04-11 16:45:03 -0300

    feat: create ButtonGroupOption component

---
+++ src/components/ButtonGroupOption.tsx
@@ -0,0 +1,39 @@
+import { Button } from "@mui/material";
+
+export interface ButtonGroupOptionSelectedCallback {
+    option: string,
+    idx: number
+}
+
+type ButtonGroupOptionProps = {
+    options: string[],
+    selectedOption: string | null,
+    onSelected: (callback: ButtonGroupOptionSelectedCallback) => void;
+}
+
+export const ButtonGroupOption = ({ options, onSelected, selectedOption }: ButtonGroupOptionProps) => {
+
+    const selectedOptionVariant = (option: string) => (selectedOption === option) ? "contained" : "outlined"
+    return (
+        <div className="buttonGroupOption">
+            {
+                options.map((option, idx) => (
+                    <Button key={idx}
+                        variant={selectedOptionVariant(option)}
+                        sx={
+                            {
+                                borderRadius: 15,
+                                width: 1,
+                                mb: idx < options.length - 1 ? 4 : 0,
+                                height: 60,
+                            }
+                        }
+                        onClick={() => onSelected({ option, idx })}
+                    >
+                        {option}
+                    </Button>
+                ))
+            }
+        </div>
+    )
+}
