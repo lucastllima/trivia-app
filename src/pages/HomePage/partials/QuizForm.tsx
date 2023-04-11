75263323b456ef75e58bf597294bd1c9f0ad38df
lucastllima <lucasteixeira-lima@hotmail.com>
2023-04-11 16:45:46 -0300

    feat(QuizForm): create initial structure with mock buttons

--- src/pages/HomePage/partials/QuizForm.tsx
+++ src/pages/HomePage/partials/QuizForm.tsx
@@ -1,3 +1,19 @@
+import { ButtonGroupOption } from '@/components';
+import { useState } from 'react';
+import {
+  CardContent,
+  Typography,
+} from "@mui/material";
+import type { ButtonGroupOptionSelectedCallback } from "@/components/ButtonGroupOption";
+
 export const QuizForm = () => {
-  return <h1>QuizForm</h1>;
+  const [options] = useState(["Answer 1", "Answer 2", "Answer 3", "Answer 4"]);
+  const [selectedOption, setSelectedOption] = useState<string | null>(null)
+
+  return (
+    <CardContent className="QuizForm">
+      <Typography variant="h6" gutterBottom>Question</Typography>
+      <ButtonGroupOption options={options} selectedOption={selectedOption} onSelected={({ option }: ButtonGroupOptionSelectedCallback) => setSelectedOption(option)} />
+    </CardContent>
+  );
 };
