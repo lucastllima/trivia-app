3bacad14d1a3470d0a43988df218ac8a88a14cbc
lucastllima <lucasteixeira-lima@hotmail.com>
2023-04-12 17:07:13 -0300

    style: fix Lint

--- src/components/ButtonGroupOption.tsx
+++ src/components/ButtonGroupOption.tsx
@@ -1,39 +1,40 @@
 import { Button } from "@mui/material";
 
 export interface ButtonGroupOptionSelectedCallback {
-    option: string,
-    idx: number
+  option: string;
+  idx: number;
 }
 
 type ButtonGroupOptionProps = {
-    options: string[],
-    selectedOption: string | null,
-    onSelected: (callback: ButtonGroupOptionSelectedCallback) => void;
-}
+  options: string[];
+  selectedOption: string | null;
+  onSelected: (callback: ButtonGroupOptionSelectedCallback) => void;
+};
 
-export const ButtonGroupOption = ({ options, onSelected, selectedOption }: ButtonGroupOptionProps) => {
-
-    const selectedOptionVariant = (option: string) => (selectedOption === option) ? "contained" : "outlined"
-    return (
-        <div className="buttonGroupOption">
-            {
-                options.map((option, idx) => (
-                    <Button key={idx}
-                        variant={selectedOptionVariant(option)}
-                        sx={
-                            {
-                                borderRadius: 15,
-                                width: 1,
-                                mb: idx < options.length - 1 ? 3 : 0,
-                                height: 60,
-                            }
-                        }
-                        onClick={() => onSelected({ option, idx })}
-                    >
-                        {option}
-                    </Button>
-                ))
-            }
-        </div>
-    )
-}
+export const ButtonGroupOption = ({
+  options,
+  onSelected,
+  selectedOption,
+}: ButtonGroupOptionProps) => {
+  const selectedOptionVariant = (option: string) =>
+    selectedOption === option ? "contained" : "outlined";
+  return (
+    <div className="buttonGroupOption">
+      {options.map((option, idx) => (
+        <Button
+          key={idx}
+          variant={selectedOptionVariant(option)}
+          sx={{
+            borderRadius: 15,
+            width: 1,
+            mb: idx < options.length - 1 ? 3 : 0,
+            height: 60,
+          }}
+          onClick={() => onSelected({ option, idx })}
+        >
+          {option}
+        </Button>
+      ))}
+    </div>
+  );
+};
--- src/pages/HomePage/partials/QuizForm.tsx
+++ src/pages/HomePage/partials/QuizForm.tsx
@@ -1,25 +1,36 @@
-import { ButtonGroupOption } from '@/components';
-import { Send } from '@mui/icons-material';
-import { useState } from 'react';
-import {
-  CardContent,
-  Typography,
-  Box,
-  Button,
-} from "@mui/material";
+import { ButtonGroupOption } from "@/components";
+import { Send } from "@mui/icons-material";
+import { useState } from "react";
+import { CardContent, Typography, Box, Button } from "@mui/material";
 import type { ButtonGroupOptionSelectedCallback } from "@/components/ButtonGroupOption";
 
 export const QuizForm = () => {
   const [options] = useState(["Answer 1", "Answer 2", "Answer 3", "Answer 4"]);
-  const [selectedOption, setSelectedOption] = useState<string | null>(null)
+  const [selectedOption, setSelectedOption] = useState<string | null>(null);
 
   return (
     <CardContent className="QuizForm">
-      <Typography variant="h6" gutterBottom>Question</Typography>
-      <ButtonGroupOption options={options} selectedOption={selectedOption} onSelected={({ option }: ButtonGroupOptionSelectedCallback) => setSelectedOption(option)} />
+      <Typography variant="h6" gutterBottom>
+        Question
+      </Typography>
+      <ButtonGroupOption
+        options={options}
+        selectedOption={selectedOption}
+        onSelected={({ option }: ButtonGroupOptionSelectedCallback) =>
+          setSelectedOption(option)
+        }
+      />
 
-      <Box mt={3} sx={{ display: 'flex', justifyContent: 'flex-end' }}>
-        <Button size="large" variant="contained" color="primary" endIcon={<Send />}>
+      <Box mt={3} sx={{ display: "flex", justifyContent: "flex-end" }}>
+        <Button
+          size="large"
+          variant="outlined"
+          color="primary"
+          sx={{
+            borderRadius: 15,
+          }}
+          endIcon={<Send />}
+        >
           Next
         </Button>
       </Box>
